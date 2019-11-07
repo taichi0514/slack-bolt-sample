fb600159e55d14f390373016002714e700b4d849
Taichi Murai <murai@tam-bourine.co.jp>
2019-11-07 17:24:08 +0900

    rm dist

--- dist/initializers/bolt.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { App } = require(`@slack/bolt`);
-const config = {
-    token: "xoxp-236314697698-235788935521-822899997124-6774c031e16a16d4ce305946c5cf49bd",
-    signingSecret: "406a5f121d080ede6f91cc362942fac5",
-};
-exports.app = new App(config);
-//# sourceMappingURL=bolt.js.map
