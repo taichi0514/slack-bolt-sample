4082561d2e9df556aae305f50bfd98a2944f4612
Taichi Murai <murai@tam-bourine.co.jp>
2019-11-07 18:33:36 +0900

    fix refactoring

--- src/commands/api.ts
+++ src/commands/api.ts
@@ -1,10 +1,11 @@
 import { app } from '../initializers/bolt'
-
+const axios = require('axios').default;
 export default function () {
-    app.command('/echo', async ({ command, ack, say }) => {
-        // コマンドリクエストを確認
-        console.log(command.text);
+    app.command('/api', async ({ ack, say }) => {
+        const url = 'https://note.mu/api/v1/users?urlname=8120001123887';
+        const response = await axios.get(url)
+        const newData = response.data.data.nickname;
         ack();
-        say(command.text);
+        say(newData);
     });
 }
--- src/commands/echo.ts
+++ src/commands/echo.ts
@@ -1,11 +1,10 @@
 import { app } from '../initializers/bolt'
-const axios = require('axios').default;
+
 export default function () {
-    app.command('/api', async ({ ack, say }) => {
-        const url = 'https://note.mu/api/v1/users?urlname=8120001123887';
-        const response = await axios.get(url)
-        const newData = response.data.data.nickname;
+    app.command('/echo', async ({ command, ack, say }) => {
+        // コマンドリクエストを確認
+        console.log(command.text);
         ack();
-        say(newData);
+        say(command.text);
     });
 }
