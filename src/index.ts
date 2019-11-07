127482bffbfd1f257b3555b42869b027914b70a2
Taichi Murai <murai@tam-bourine.co.jp>
2019-11-07 18:39:44 +0900

    fix refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { app } from '../src/initializers/bolt'
-import Hello from '../src/message/hello'
-import Echo from '../src/commands/echo'
-import Api from '../src/commands/api'
+import { app } from './initializers/bolt'
+import Hello from './message/hello'
+import Echo from './commands/echo'
+import Api from './commands/api'
 
 (async () => {
     const server = await app.start(process.env.PORT || 3000);
